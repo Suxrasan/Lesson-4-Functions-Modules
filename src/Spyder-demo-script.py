6ac5d4c952e51815b1fff8acb93c4368e681a349
Suxrasan <susanna.haanpaa@helsinki.fi>
2016-10-02 19:29:37 +0300

    Fahrenheits to kelvin

--- src/Spyder-demo-script.py
+++ src/Spyder-demo-script.py
@@ -1,32 +1,37 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Spyder-demo-script.py
 
-This script defines a few variables to demonstrate using the Spyder IDE's
-object inspector and variable explorer.
+def celisius_to_farh(temp_c):
+#Function to convert Celsius temperature to Fahrenheit. 
+	return 9/5 * temp_c + 32 
 
-Created on Sat Sep 24 23:23:11 2016
+def kelvin_to_celsius(temp_k):
+#Function to convert Kelvin temperature to Celsius.
+	return temp_k - 273.15
 
-@author: David Whipp
-"""
+def kelvin_to_farhenheit(temp_k):
+#Function to convert Kelvin temperature to Fahrenheit. 
+	#Kelvin in celsius
+	temp_c = kelvin_to_celsius(temp_k)
+	#Celsius in Fahrenheit
+	temp_f = celsius_to_fahr(temp_c)
+	#Return the result
+	return temp_f
 
-# Define a few variables
-BigNumber = 123456789
-BigFloat = 123456789.0123456789
-DummyString = "Tropical Hot Dog Night"
-ChocolateIsGood = 3
+def celsius_to_kelvin(temp_c): 
+	#Kelvin = Celsius - 273.15 
+	temp_k = temp_c + 273.15
+	return temp_k
 
-# Define a line containing the variables above
-OddList = [BigNumber, BigFloat, DummyString, ChocolateIsGood]
+def fahr_to_celsius(temp_f):
+	#Celsius = (Fahrenheit - 32) * 5/9
+	temp_c = (temp_f - 32) * 5/9 
+	return temp_c
 
-# Modify one variable (note the value for this in the list)
-BigNumber = 1048576    # One megabyte in bytes
+def fahr_to_kelvin(temp_f):
+	# Kelvin = (Fahrenheit - 459.67) * 5/9
+	temp_k = (temp_f + 459.67) * 5/9
+	return temp_k
 
-# Print the current value of BigNumber
-print("BigNumber is now", BigNumber)
 
-# Print BigNumber in binary
-print("BigNumber is binary is", bin(BigNumber))
-
-# Add print statement here to day hello
+print('32 Fahrenheits is in Celsius:', fahr_to_celsius(32))
+print('100 Celsius is in Kelvin:', celsius_to_kelvin(temp_c=100))
+print('50 Fahrenheits is in Kelvin:', fahr_to_kelvin(temp_f=50))
